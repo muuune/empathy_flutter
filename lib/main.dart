bf65f9280739063ae53cd917278a256f2642c406
MasaMune-68 <masamunedazeze@gmail.com>
2023-02-17 15:41:58 +0900

    FirebaseAuthでログイン後の状態を維持するよう実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,12 @@
 import 'package:empathy_flutter/pages/home_page.dart';
 import 'package:empathy_flutter/pages/login_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -16,13 +14,33 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: LoginPage(),
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'Flutter app',
+        home: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              // スプラッシュ画面などに書き換えても良い
+              return const SizedBox();
+            }
+            if (snapshot.hasData) {
+              // User が null でなない、つまりサインイン済みのホーム画面へ
+              return const HomePage();
+            }
+            // User が null である、つまり未サインインのサインイン画面へ
+            return LoginPage();
+          },
+        ),
+      );
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return MaterialApp(
+  //     title: 'Flutter Demo',
+  //     theme: ThemeData(
+  //       primarySwatch: Colors.blue,
+  //     ),
+  //     home: LoginPage(),
+  //   );
+  // }
 }
