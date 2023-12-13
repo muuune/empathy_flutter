17659d761ee1909d672c107a6c1ef4b04ea3b084
Masamune Takahashi <masamunedazeze@gmail.com>
2023-12-13 16:43:03 +0900

    [#97]深刻な悩みを抱えている方向けの窓口を紹介する

--- lib/pages/tutorial_page.dart
+++ lib/pages/tutorial_page.dart
@@ -33,42 +33,32 @@ class TutorialPage extends StatelessWidget {
     PageModel(
         color: const Color.fromARGB(255, 81, 161, 101),
         imageAssetPath: 'images/logo.png',
-        title: 'アナタノミカタへようこそ！',
-        body:
-            'このアプリでは、あなたと同じ悩みを抱える方と繋がることができます。\nここでは簡単に、アプリの流れや機能について説明していきます！',
+        title: 'アナタノミカタへ\nようこそ！',
+        body: 'このアプリでは、あなたと同じ悩みを抱える方と繋がることができます。\n流れを簡単に説明していきます。',
         doAnimateImage: true),
     PageModel(
         color: const Color.fromARGB(255, 69, 142, 88),
         imageAssetPath: 'images/registerpage.png',
-        title: '1. あなたのお悩みを登録',
-        body: 'ユーザー登録時に、あなたの悩みに該当する項目を選択していただくことで、悩みを登録していきます。',
+        title: '1. 悩みを登録',
+        body: 'あなたの悩みに該当する項目を選択することで、悩みを登録していきます。',
         doAnimateImage: true),
     PageModel(
         color: const Color.fromARGB(255, 81, 161, 101),
         imageAssetPath: 'images/homepage_sample01.png',
-        title: '2. あなたと同じ悩みを抱えるお相手が表示されます',
-        body:
-            '4つの悩み要因ごとに、同じ悩みを抱えるお相手が表示されます。\nマッチング相手をタップすると、登録されている悩みの説明を見ることができます！',
-        doAnimateImage: true),
-    PageModel(
-        color: const Color.fromARGB(255, 81, 161, 101),
-        imageAssetPath: 'images/homepage_sample02.png',
-        title: '2. あなたと同じ悩みを抱えるお相手が表示されます',
-        body: 'また、左上のメニューから、登録した悩みの確認や、悩みの説明の編集等をすることができます！',
+        title: '2. 同じ悩みを抱える相手を表示',
+        body: 'マッチング相手をタップすると、悩みの説明を見ることができます。',
         doAnimateImage: true),
     PageModel(
         color: const Color.fromARGB(255, 69, 142, 88),
         imageAssetPath: 'images/line_openchat.png',
-        title: '3. LINEのオープンチャットで1日限りのトーク',
-        body:
-            'マッチングしたお相手と1対1で、1日限りのトークをしていただきます。\nトークルームの招待リンクは、後ほど運営からメールで届きます。',
+        title: '3. LINEのオープンチャットで一日限りのトーク',
+        body: 'マッチングした相手と1対1で、一日限りのトークをしていただきます。',
         doAnimateImage: true),
     PageModel(
         color: const Color.fromARGB(255, 81, 161, 101),
         imageAssetPath: 'images/OK.png',
         title: '⚠️ トーク時の注意',
-        body:
-            'トークは1日限りのため、積極的な交流をお願い致します。\nまた、お相手とのトーク時は、否定ではなく「共感」をするように心がけましょう！',
+        body: 'トークは一日限りのため、積極的な交流をお願いします。\nまた、トーク時は相手の悩みに共感するよう心がけましょう！',
         doAnimateImage: true),
     // PageModel.withChild(
     //     child: const Padding(
